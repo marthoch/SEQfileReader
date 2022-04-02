47276e6310dadf315a5e47d5420d33daccf9257b
Martin Hochwallner <marthoch@users.noreply.github.com>
2022-04-02 18:23:06 +0200

    added read_line

--- src/SEQfileReader/SEQfileReader.py
+++ src/SEQfileReader/SEQfileReader.py
@@ -6,6 +6,12 @@ __license__ = "BSD 3-clause"
 import numpy as np
 import datetime
 import pandas as pd
+from numpy import ndarray
+import matplotlib.pyplot as plt
+
+import logging
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
 
 try:
     import fnv
@@ -14,9 +20,10 @@ try:
     # documentation file:///C:/Program%20Files/FLIR%20Systems/sdks/file/python/doc/index.html
 except Exception as e:
     print("""
-download "FLIR Science File SDK" from https://flir.custhelp.com/app/account/fl_download_software
-install Python module according https://flir.custhelp.com/app/answers/detail/a_id/3504/~/getting-started-with-flir-science-file-sdk-for-python
-    Use the setup.py attached to the installation help.
+## Installation of the fnv module:
+* Download and install the "FLIR Science File SDK" from https://flir.custhelp.com/app/account/fl_download_software.
+* Install Python module according https://flir.custhelp.com/app/answers/detail/a_id/3504/~/getting-started-with-flir-science-file-sdk-for-python  
+    Attention: Use the setup.py attached to the "Getting started with ..."  as the one in the SDK is broken.        
 """)
     raise e
 
@@ -46,6 +53,7 @@ SEQfile(filename=r'testRecording.seq')
     frameRate_nominal_available = 200. / 2 ** np.arange(0, 5)
 
     def __init__(self, filename):
+        log.debug('opening "%s"', filename)
         self.filename = filename
         self.im = fnv.file.ImagerFile(self.filename)
         self.im.unit = fnv.Unit.TEMPERATURE_FACTORY
@@ -66,16 +74,47 @@ SEQfile(filename=r'testRecording.seq')
 
         self.timeArray = None
 
+    @property
+    def number_of_frames(self):
+        """ Number of Frames in the SEQ file
+        """
+        return self.im.num_frames
+
+    @property
+    def current_frame_number(self):
+        return self.im.frame_number
+
+    @property
+    def current_frame_time(self):
+        return self.im.frame_info.time
+
+    def go2next_frame(self, step=1):
+        try:
+            self.go2frame(self.current_frame_number + step)
+        except IndexError:
+            raise StopIteration
+
+    def go2frame(self, frame_number):
+        if 0 > frame_number or frame_number >= self.im.num_frames:
+            raise IndexError('Out of range: {} not it [0..{}]'.format(frame_number, self.im.num_frames - 1))
+        else:  # return value of get_frame is not reliable
+            if not self.im.get_frame(frame_number):
+                self.im.get_frame(0)
+                raise IndexError('Out of range: {} not it [0..{}]'.format(frame_number, self.im.num_frames - 1))
+
+    def frame_iter(self, start=0, stop=None, step=1):
+        return FrameIterator(seq=self, start=start, stop=stop, step=step)
+
     def __str__(self):
         return """SEQfileReader(filename=r'{s.filename}')
     unit: ............... {s.im.unit.name} / {s.im.temp_type.name} 
     image size: ......... {s.im.width} x {s.im.height}
-    number of frames: ... {s.im.num_frames}
+    number of frames: ... {s.number_of_frames}
     recording start time: {recStartTime}
     recording time: ..... {recordingTime} sec
     frame rate: ......... {s.frameRate} Hz
     current frame:
-        frame number: ....... {s.im.frame_number}
+        frame number: ....... {s.current_frame_number}
         preset number: ...... {s.im.num_presets}
         date/time of frame:   {frame_info_time}
         """.format(s=self,
@@ -87,6 +126,9 @@ SEQfile(filename=r'testRecording.seq')
 
     def get_image(self):
         return np.array(self.im.final, copy=False).reshape((self.im.height, self.im.width))
+
+    def set_active_frame(self, fn):
+        return self.im.get_frame(fn)
 
     def get_time_df_nom(self):
         """Time vector nominally spaced (based on frame rate) as pandas DataFrame."""
@@ -100,14 +142,16 @@ SEQfile(filename=r'testRecording.seq')
     def read_time_df(self, reread=False):
         if reread or (self.timeArray is None):
             t = np.empty(self.im.num_frames, dtype=datetime.datetime)
-            for fr in self.im.frame_iter(0):
-                t[fr.frame_number] = fr.frame_info.time
-            self.timeArray = pd.DataFrame({'time_file': t})
+            fi: ndarray = np.empty(self.im.num_frames, dtype=np.uint32)
+            for i, fr in enumerate(self.im.frame_iter(0)):
+                t[i] = fr.frame_info.time
+                fi[i] = fr.frame_number
+            self.timeArray = pd.DataFrame({'time_file': t, 'fn': fi})
         return self.timeArray
 
-    def read_full_numpy(self, selection=None, frame_range=None):
+    def read_as_numpy(self, selection=None, frame_range=None):
         """
-        selection=[x1,y1, x2,y2]
+        selection=[[h0, h1],[w0, w1]] ... [[height from to},[width from to]]
         frameRange=[startFrame, endFrame]
         """
         if frame_range is None:
@@ -119,8 +163,8 @@ SEQfile(filename=r'testRecording.seq')
             h = self.im.height
             w = self.im.width
         else:
-            h = selection[3] - selection[1]
-            w = selection[2] - selection[0]
+            h = selection[0][1] - selection[0][0]
+            w = selection[1][1] - selection[1][0]
 
         # preallocate arrays
         fi = np.empty(n_frames, dtype=np.uint32)
@@ -137,9 +181,157 @@ SEQfile(filename=r'testRecording.seq')
             if selection is None:
                 v[:, :, i] = img
             else:
-                v[:, :, i] = img[selection[1]:selection[3], selection[0]:selection[2]]
+                v[:, :, i] = img[selection[0][0]:selection[0][1], selection[1][0]:selection[1][1]]
         if frame_range is None:  # when reading all
             self.timeArray = pd.DataFrame({'time': t})
         return t, v, fi
+
+    def read_line(self, line=None, hline=None, vline=None, interpolation='linear'):
+        """
+        line = dict(p0=dict(v=1, h=1), p1=dict(v=10, h=50), len=None)
+        interpolation='linear' or 'nearest'
+        """
+        img = self.get_image()
+        # img = np.ones([100,100])
+        # for i in np.arange(0,100):
+        #    for j in np.arange(0,100):
+        #        img[i,j] = i
+        if hline is not None:
+            if (hline % 1) == 0.0:
+                val = img[int(hline), :]
+                v = h = None
+                return dict(v=v, h=h, values=val)
+            else:
+                if interpolation == 'linear':
+                    a = (hline % 1)
+                    l = int(hline)
+                    val = (1 - a) * img[l, :] + a * img[int(l + 1), :]
+                    v = h = None
+                    return dict(v=v, h=h, values=val)
+                elif interpolation == 'nearest':
+                    l = int(round(hline))
+                    v = h = None
+                    return dict(v=v, h=h, values=img[l, :])
+                else:
+                    raise Exception("interpolation not known: use interpolation='linear' or 'nearest'")
+        if vline is not None:
+            if (vline % 1) == 0.0:
+                return img[:, int(vline)]
+            else:
+                if interpolation == 'linear':
+                    a = (vline % 1)
+                    l = int(vline)
+                    return (1 - a) * img[:, int(vline)] + a * img[:, int(vline + 1)]
+                elif interpolation == 'nearest':
+                    l = int(round(vline))
+                    v = h = None
+                    return dict(v=v, h=h, values=img[:, l])
+                else:
+                    raise Exception("interpolation not known: use interpolation='linear' or 'nearest'")
+        if line is not None:
+            lineLen = line.get('len')
+            lineLenX = max(abs(line['p1']['v'] - line['p0']['v']), abs(line['p1']['h'] - line['p0']['h'])) + 1
+            if lineLen is None:
+                lineLen = lineLenX
+            if lineLen < lineLenX:
+                raise Exception("down sampling interpolation is not implemented")
+            v = np.linspace(line['p0']['v'], line['p1']['v'], lineLen)
+            h = np.linspace(line['p0']['h'], line['p1']['h'], lineLen)
+            if interpolation == 'linear':
+                vi = v.astype(int)
+                hi = h.astype(int)
+                va = v - vi
+                ha = h - hi
+                val = (1 - va) * (1 - ha) * img[vi, hi] + va * (1 - ha) * img[vi + 1, hi] + (1 - va) * ha * img[
+                    vi, hi + 1] + va * ha * img[vi + 1, hi + 1]
+            elif interpolation == 'nearest':
+                vi = v.round(decimals=0).astype(int)
+                hi = h.round(decimals=0).astype(int)
+                val = img[vi, hi]
+            else:
+                raise Exception("interpolation not known: use interpolation='linear' or 'nearest'")
+            return dict(v=v, h=h, values=val)
+        raise Exception('not enough parameters')
+
+    def read_line_over_time(self, start=0, stop=None, step=1, line=None, hline=None, vline=None,
+                            interpolation='linear', plot_show_line=None):
+        """
+
+        :param start:
+        :param stop:
+        :param step:
+        :param line:  dict(p0=dict(v=1, h=1), p1=dict(v=10, h=50), len=None)
+        :param hline:
+        :param vline:
+        :param interpolation:  'linear' or 'nearest'
+        :param plot_show_line: plot one image and show line, if Ture show current frame, if int shaw that frame
+        :return:
+        """
+
+        if plot_show_line is not None:
+            if plot_show_line is True:
+                self.go2frame(0)
+            else:
+                self.go2frame(plot_show_line)
+            fig, ax = plt.subplots(1, 1)
+            ax.imshow(self.get_image() - 273.15, interpolation='nearest', cmap='plasma')
+            ax.plot([line['p0']['h'], line['p1']['h']], [line['p0']['v'], line['p1']['v']],
+                    color='lime', lw=2, alpha=0.7)
+            ax.plot([line['p0']['h'], ], [line['p0']['v'], ], color='lime', lw=2, alpha=0.7, marker='o',
+                    markersize=15)
+            ax.plot([line['p1']['h'], ], [line['p1']['v'], ], color='deepskyblue', lw=2, alpha=0.7,
+                    marker='s', markersize=15)
+            return fig
+
+        # preallocate arrays
+        stop_ = stop if stop is not None else self.number_of_frames
+        n_frames = (stop_ - start) // step
+        fi = np.empty(n_frames, dtype=np.uint32)
+        t = np.empty(n_frames, dtype=datetime.datetime)
+        self.go2frame(0)
+        aline = self.read_line(line=line, hline=hline, vline=vline, interpolation=interpolation)
+        v = np.empty([aline['values'].shape[0], n_frames], dtype=np.float32)
+
+        for i, seqFrame in enumerate(self.frame_iter(start=start, stop=stop, step=step)):
+            # display(i)
+            fi[i] = seqFrame.current_frame_number
+            t[i] = seqFrame.current_frame_time
+            v[:, i] = self.read_line(line=line, hline=hline, vline=vline, interpolation=interpolation)['values']
+
+        return dict(time=t, values=v, frame_number=fi)
+
+
+class FrameIterator:
+    """Iterator over Frames
+    It acts on the SEQ's current frame thus there can be only one iterator active at the same time.
+    """
+
+    def __init__(self, seq, start=0, stop=None, step=1):
+        self.seq = seq
+        self.start = start
+        self.stop = stop
+        self.step = step
+        self.firstIter = None
+
+    def __iter__(self):
+        self.seq.go2frame(self.start)
+        self.firstIter = True
+        return self
+
+    def __next__(self):
+        try:
+            if self.firstIter:
+                self.firstIter = False
+                return self.seq
+            if (self.stop is None) or (self.seq.current_frame_number + self.step < self.stop):
+                self.seq.go2next_frame(self.step)
+                return self.seq
+            else:
+                raise StopIteration
+        except StopIteration:
+            raise
+        except Exception as e:
+            log.error(e)
+            raise StopIteration
 
 # eof
